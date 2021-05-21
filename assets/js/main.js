5428418f70b7e38b5d737eda8d198b7cb7e58cd3
pavel <geraci98.98@gmail.com>
2021-05-21 18:23:52 +0300

    init

---
+++ assets/js/main.js
@@ -0,0 +1,63 @@
+//function mainSection() {
+//     
+//}
+
+
+//$(".js-open-nav").click(function () {
+//    $("nav").addClass("active");
+//});
+
+
+//let canvasMain = document.getElementById("canvas-main");
+//let ctx = canvasMain.getContext("2d");
+//
+//ctx.beginPath();
+//ctx.strokeStyle = ("white");
+//ctx.lineWidth = "1";
+//ctx.moveTo(0, 100);
+//ctx.lineTo(30, 0);
+//ctx.stroke();
+//
+//ctx.beginPath();
+//ctx.strokeStyle = ("white");
+//ctx.lineWidth = "1";
+//ctx.moveTo(0, 100);
+//ctx.lineTo(65, 0);
+//ctx.stroke();
+//
+//ctx.beginPath();
+//ctx.strokeStyle = ("white");
+//ctx.lineWidth = "1";
+//ctx.moveTo(0, 100);
+//ctx.lineTo(100, 10);
+//ctx.stroke();
+//
+//ctx.beginPath();
+//ctx.strokeStyle = ("white");
+//ctx.lineWidth = "1";
+//ctx.moveTo(0, 100);
+//ctx.lineTo(100, 70);
+//ctx.stroke();
+
+
+//$("main").click(function ()  {
+//    $("nav").removeClass("active");
+//});
+
+//$(".js-open-сontact").mouseover(function () {
+//    $("section").removeClass("active");
+//    $(".сontact").addClass("active");
+//    $(".main-nav").addClass("active");
+//});
+//
+//$(".js-open-works").mouseover(function () {
+//    $("section").removeClass("active");
+//    $(".works").addClass("active");
+//    $(".main-nav").addClass("active");
+//});
+//
+//$(".js-open-main").mouseover(function () {
+//    $("section").removeClass("active");
+//    $(".main-screen").addClass("active");
+//    $(".main-nav").removeClass("active");
+//});
